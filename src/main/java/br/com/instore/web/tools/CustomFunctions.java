d9abbd81f7c68535fe77a2e63ff0a188c00763d8
instorecode <alex.goncalves@instore.com.br>
2014-08-05 17:49:01 -0300

    05.07.2014

--- src/main/java/br/com/instore/web/tools/CustomFunctions.java
+++ src/main/java/br/com/instore/web/tools/CustomFunctions.java
@@ -1,22 +1,33 @@
 package br.com.instore.web.tools;
 
+import com.ibm.icu.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.commons.lang.StringUtils;
 
 public class CustomFunctions {
-    public static String dateFormat(Date date , String format) {
-        if( null != date ) {
+
+    public static String dateFormat(Date date, String format) {
+        if (null != date) {
             return new SimpleDateFormat(format).format(date);
-        } else if( null == date ) {
-            return new SimpleDateFormat(format).format( new Date());
+        } else if (null == date) {
+            return new SimpleDateFormat(format).format(new Date());
         }
         return "";
     }
+
     public static String dateCurrent(String format) {
-        return new SimpleDateFormat(format).format( new Date());
+        return new SimpleDateFormat(format).format(new Date());
     }
-    public static String leftPad(String value , int size, String value2) {
+
+    public static String leftPad(String value, int size, String value2) {
         return StringUtils.leftPad(value, size, value2);
+    }
+
+    public static String dateAfterOneYear() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.YEAR, 1);
+        return new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime());
     }
 }
