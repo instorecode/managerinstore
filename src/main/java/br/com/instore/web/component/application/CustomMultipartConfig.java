16cd49dcd6c8a3406ce6569fc832477c8a559f4a
instorecode <alex.goncalves@instore.com.br>
2015-10-21 10:37:57 -0200

    limpar projeto

--- src/main/java/br/com/instore/web/component/application/CustomMultipartConfig.java
+++ src/main/java/br/com/instore/web/component/application/CustomMultipartConfig.java
@@ -1,17 +1,17 @@
 package br.com.instore.web.component.application;
 
 import br.com.caelum.vraptor.interceptor.multipart.DefaultMultipartConfig;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+import br.com.caelum.vraptor.ioc.Component;
 
-
-
-
-
+@Component
+@ApplicationScoped
 public class CustomMultipartConfig extends DefaultMultipartConfig {
 
     public long getFileSizeLimit() {
         return 50 * 1024 * 1024; // 50MB
     }
-    
+
     public long getSizeLimit() {
         return 50 * 1024 * 1024; // 50MB
     }
